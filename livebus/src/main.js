914d783d3c792465f95726ea232cd1db986e906e
shankha117 <sinha.shankha17@gmail.com>
2020-07-20 00:03:52 +0530

    client

--- livebus/src/main.js
+++ livebus/src/main.js
@@ -1,12 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
-import store from "./store";
-import vuetify from './plugins/vuetify';
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import store from "./store/store";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-Vue.config.productionTip = false;
-
+delete L.Icon.Default.prototype._getIconUrl;
+// eslint-disable-next-line
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
 new Vue({
-  store,
+  router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount("#app");
